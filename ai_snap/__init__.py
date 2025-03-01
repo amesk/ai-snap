c6e8879125e2756cc307d0b7b14fd9f967906ff7
amesk <alexei.eskenazi@evomarine.ru>
2025-03-01 23:23:56 +0300

    Added formal variables to __init__.py.

--- ai_snap/__init__.py
+++ ai_snap/__init__.py
@@ -9,3 +9,12 @@
 #
 
 from .cli import cli
+
+try:
+    from .version import VERSION as __version__
+except ModuleNotFoundError:
+    __version__ = "0.0.1"
+
+__author__ = "amesk <alexei.eskenazi@gmail.com>"
+__license__ = "MIT"
+__all__ = ["cli"]
